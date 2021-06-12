9371773358ae73382f656075f762cc5c4f902c91
satoshi.okada <ubuntu@ip-172-31-10-77.ap-northeast-1.compute.internal>
2021-06-12 10:01:34 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ Route::get('/', 'MicropostsController@index');
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::group(['middleware' => ['auth']], function () {
+    Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', 'UserFollowController@store')->name('user.follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+        Route::get('followings', 'UsersController@followings')->name('users.followings');
+        Route::get('followers', 'UsersController@followers')->name('users.followers');
+    });
+
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
